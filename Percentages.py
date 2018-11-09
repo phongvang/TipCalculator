bf4855f65da227b7782a23dfd15d3b38237b6668
Phong Vang <phong.vang5@gmail.com>
2018-11-08 22:04:28 -0600

    Updated
    Added modules.
    improved code.
    truncated amounts to 2nd decimal spaces.

--- Percentages.py
+++ Percentages.py
@@ -1,16 +1,36 @@
+#modules used in tip calculator program
+
 import math
 
+def truncate(amount,n):
+    return math.floor(amount * 10 ** n) / 10 ** n
+
 def fifteenPercent(billAmount):
-    fifteenPercent = .15
-    totalBill15 = billAmount + fifteenPercent
+    n = 2
+    fifteenPercent = 0.15
+    tip = billAmount * fifteenPercent
+    total = billAmount + tip
+
+    print("\n15%% tip = $", truncate(tip,n))
+    print("new total = $", truncate(total,n))
 
 
 def eighteenPercent(billAmount):
-    eighteenPercent = .18
-    totalBill18 = billAmount + eighteenPercent
+    n = 2
+    eighteenPercent = 0.18
+    tip = billAmount * eighteenPercent
+    total = billAmount + eighteenPercent
+
+    print("\n18%% tip = $", truncate(tip,n))
+    print("new total = $", truncate(total,n))
 
 
 def twentyPercent(billAmount):
-    twentyPercent = .20
-    totalBill20 = billAmount + twentyPercent
+    n = 2
+    twentyPercent = 0.20
+    tip = billAmount * twentyPercent
+    total = billAmount + tip
+
+    print("\n20%% tip = $", truncate(tip,n))
+    print("new total = $", truncate(total,n))
 
--- TipCalculator.py
+++ TipCalculator.py
@@ -1,6 +1,6 @@
 #Author: Phong Vang
 #Date: 11.08.2018
-#This program calculates a tip for 15%, 18%, and 20% of an amount.
+#This program calculates a tip for 15%, 18%, and 20% of an input amount.
 
 import Percentages
 
@@ -9,26 +9,13 @@ def main():
     print("Welcome to Tip Calculator program.")
     print("This program calculates the tip for a given amount.")
 
-    billAmount = eval( input ("\nEnter an amount: $") );
+    billAmount = eval( input ("\nEnter an amount: $"));
 
-    fifteenPercent = billAmount * .15
-    totalBill1 = billAmount + fifteenPercent
-            
-    eighteenPercent = billAmount * .18
-    totalBill2 = billAmount + eighteenPercent
-                       
-    twentyPercent = billAmount * .20
-    totalBill3 = billAmount + twentyPercent
+    Percentages.fifteenPercent(billAmount)
+    Percentages.eighteenPercent(billAmount)
+    Percentages.twentyPercent(billAmount)
 
-    print("\n(15% tip): $", fifteenPercent)
-    print("The total is $%.2f" % (totalBill1))
 
-    print("\n(20% tip): $",eighteenPercent)
-    print("The total is $%.2f" % (totalBill2))
-
-    print("\n(25% tip): $",twentyPercent)
-    print("The total is $%.2f" % (totalBill3))
-
-    print("\n\nThank you for using this program!")
+    print("\n\nHappy Tipping. End of Program.")
 
 main()
